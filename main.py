143e8fd97c3c9ca4ddacf7de8c75826d857e129f
KrisRock <hatebreader@yandex.ru>
2025-02-03 20:44:51 +0300

    Добавлен автодебаг

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import os
 import time
-import datetime
-import requests
 from icecream import ic
-from bs4 import BeautifulSoup
-from settings import filmitorrent, films_list_base, DEBUG
 from dotenv import load_dotenv
-
-from send_to_telegram import telegram_sender
 from parce_list import parse_page
-
+from send_to_telegram import telegram_sender
 
 # Загрузить переменные окружения из .env
 load_dotenv()
 
 
 def main():
+    DEBUG = bool(os.getenv("DEBUG"))
+
     DELAY = 3600
     try:
         DELAY = int(os.getenv("DELAY"))
@@ -26,8 +22,8 @@ def main():
     ic(DEBUG)
 
     while True:
-        parse_page()
-        telegram_sender()
+        # parse_page()
+        # telegram_sender()
         if not DEBUG:
             time.sleep(DELAY)
         else:
